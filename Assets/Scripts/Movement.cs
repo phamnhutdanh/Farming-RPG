108e283632a9f85b0d7ff4c7d7552bd0688c2fc2
Danh <nhutdanh175@gmail.com>
2024-06-21 09:29:09 +0700

    init

---
+++ Assets/Scripts/Movement.cs
@@ -0,0 +1,36 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Movement : MonoBehaviour
+{
+    public float speed;
+    public Animator animator;
+
+    private void Update()
+    {
+        float horizontal = Input.GetAxisRaw("Horizontal");
+        float vertical = Input.GetAxisRaw("Vertical");
+
+        Vector3 direction = new Vector3(horizontal, vertical);
+        AnimateMovement(direction);
+        transform.position += direction * Time.deltaTime * speed;
+    }
+
+    void AnimateMovement(Vector3 direction)
+    {
+        if (animator != null)
+        {
+            if (direction.magnitude > 0)
+            {
+                animator.SetBool("isWalking", true);
+                animator.SetFloat("horizontal", direction.x);
+                animator.SetFloat("vertical", direction.y);
+            }
+            else
+            {
+                animator.SetBool("isWalking", false);
+            }
+        }
+    }
+}
