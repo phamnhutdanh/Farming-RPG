10d046ad8326cec2a218eb0b75ac3cfc87360993
Danh <nhutdanh175@gmail.com>
2024-06-26 23:58:14 +0700

    setup tool bar

--- Assets/Scripts/UI/Slot_UI.cs
+++ Assets/Scripts/UI/Slot_UI.cs
@@ -9,6 +9,9 @@ public class Slot_UI : MonoBehaviour
     public Image itemIcon;
     public TextMeshProUGUI quantityText;
 
+    [SerializeField]
+    private GameObject highlight;
+
     public void SetItem(Inventory.Slot slot)
     {
         if (slot != null)
@@ -24,5 +27,10 @@ public class Slot_UI : MonoBehaviour
         itemIcon.sprite = null;
         itemIcon.color = new Color(1, 1, 1, 0);
         quantityText.text = "";
+    }
+
+    public void SetHighlight(bool isOn)
+    {
+        highlight.SetActive(isOn);
     }
 }
